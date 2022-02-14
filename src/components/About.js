aafe62cb4bc9dce8f13ac03de20cc9f93041fa70
adriencyberspace <60076088+adriencyberspace@users.noreply.github.com>
2022-02-14 11:19:43 -0600

    summer

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ const About = () => {
             <div>
                 <P style={marginTop20}>Mildew is a print magazine about secondhand fashion and creative reuse, featuring art and writing that inspires us to think about old clothes in new ways. As the voice of the new-to-you ecosystem, Mildew is a guide to dressing in the spirit of our times and reveling in the decay of fashion as we knew it.</P>
                 <P style={marginTop20}>Not new, but original.</P>
-                <P style={marginTop20}>In print Spring 2022.</P>
+                <P style={marginTop20}>In print Summer 2022.</P>
                 <Span>
                     <P style={marginTop20}><MobileLink to="/">Home</MobileLink></P>
                 </Span>
