a03f5e32a08bdc6fc350320c7538d7cd3891cca6
Adrien Young <adrien.elizabeth@gmail.com>
2022-08-30 12:16:54 -0500

    october

--- src/components/About.js
+++ src/components/About.js
@@ -1,25 +1,31 @@
-import React from 'react'
-import { P, Span, MobileLink } from './Content'
+import React from "react";
+import { P, Span, MobileLink } from "./Content";
 
 const About = () => {
-    const marginTop20 = {
-        marginTop: 20
-    }
-    
-    return (
-        <div className="content-container">
-            <div>
-                <P style={marginTop20}>Mildew is a print magazine about secondhand fashion and creative reuse, featuring art and writing that inspires us to think about old clothes in new ways. As the voice of the new-to-you ecosystem, Mildew is a guide to dressing in the spirit of our times and reveling in the decay of fashion as we knew it.</P>
-                <P style={marginTop20}>Not new, but original.</P>
-                <P style={marginTop20}>In print Summer 2022.</P>
-                <Span>
-                    <P style={marginTop20}><MobileLink to="/">Home</MobileLink></P>
-                </Span>
-            </div>
-        </div>
+  const marginTop20 = {
+    marginTop: 20,
+  };
 
-    )
+  return (
+    <div className="content-container">
+      <div>
+        <P style={marginTop20}>
+          Mildew is a print magazine about secondhand fashion and creative
+          reuse, featuring art and writing that inspires us to think about old
+          clothes in new ways. As the voice of the new-to-you ecosystem, Mildew
+          is a guide to dressing in the spirit of our times and reveling in the
+          decay of fashion as we knew it.
+        </P>
+        <P style={marginTop20}>Not new, but original.</P>
+        <P style={marginTop20}>In print October 2022.</P>
+        <Span>
+          <P style={marginTop20}>
+            <MobileLink to="/">Home</MobileLink>
+          </P>
+        </Span>
+      </div>
+    </div>
+  );
+};
 
-}
-
-export { About }
+export { About };
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,48 @@
-import React from 'react'
+import React from "react";
 
-import Mailchimp from './Mailchimp'
-import { A, Blurb, BlurbDiv, P, Span, MobileLink } from './Content'
+import Mailchimp from "./Mailchimp";
+import { A, Blurb, BlurbDiv, P, Span, MobileLink } from "./Content";
 
 const Home = () => {
-    const marginTop20 = {
-        marginTop: 20
-    }
+  const marginTop20 = {
+    marginTop: 20,
+  };
 
-    const marginBottom20 = {
-        marginBottom: 20
-    }
+  const marginBottom20 = {
+    marginBottom: 20,
+  };
 
-    return (
-        <div className="content-container">
-            <div>
-                <BlurbDiv>
-                    <Blurb> The secondhand fashion magazine.</Blurb>
-                </BlurbDiv>
-                <P style={marginBottom20}> In print Summer 2022. </P>   
-            </div>
-            <P style={marginBottom20}> Stay in the loop:</P>
-                <Mailchimp />
-            <Span>
-                <P style={marginTop20}><A rel="noreferrer" target="_blank" href="https://www.instagram.com/mildewmag">@mildewmag</A></P>
-                <P style={marginTop20}><A rel="noreferrer" target="_blank" href="mailto:mildewmag@gmail.com">mildewmag@gmail.com</A></P>
-                <P style={marginTop20}><MobileLink to="/about">About Mildew</MobileLink></P>
-            </Span>
-        </div>
-    )
-}
+  return (
+    <div className="content-container">
+      <div>
+        <BlurbDiv>
+          <Blurb> The secondhand fashion magazine.</Blurb>
+        </BlurbDiv>
+        <P style={marginBottom20}> In print October 2022. </P>
+      </div>
+      <P style={marginBottom20}> Stay in the loop:</P>
+      <Mailchimp />
+      <Span>
+        <P style={marginTop20}>
+          <A
+            rel="noreferrer"
+            target="_blank"
+            href="https://www.instagram.com/mildewmag"
+          >
+            @mildewmag
+          </A>
+        </P>
+        <P style={marginTop20}>
+          <A rel="noreferrer" target="_blank" href="mailto:mildewmag@gmail.com">
+            mildewmag@gmail.com
+          </A>
+        </P>
+        <P style={marginTop20}>
+          <MobileLink to="/about">About Mildew</MobileLink>
+        </P>
+      </Span>
+    </div>
+  );
+};
 
-export { Home }
+export { Home };
